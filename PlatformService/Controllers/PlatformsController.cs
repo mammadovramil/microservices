12e743c67884cdee349fbccffc6239f1d8cefc7d
Ramil Mammadov <ramil2319@gmail.com>
2023-01-12 14:22:58 +0400

    Added Message Bus Publisher to Platform Service

--- PlatformService/Controllers/PlatformsController.cs
+++ PlatformService/Controllers/PlatformsController.cs
@@ -1,5 +1,6 @@
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using PlatformService.AsyncDataServices;
 using PlatformService.Data;
 using PlatformService.Dtos;
 using PlatformService.Models;
@@ -14,15 +15,19 @@ namespace PlatformService.Controllers
         private readonly IPlatformRepo _repository;
         private readonly IMapper _mapper;
         private readonly ICommandDataClient _commandDataClient;
+        private readonly IMessageBusClient _messageBusClient;
 
         public PlatformsController(
             IPlatformRepo repository,
             IMapper mapper,
-            ICommandDataClient commandDataClient)
+            ICommandDataClient commandDataClient,
+            IMessageBusClient messageBusClient
+            )
         {
             _repository = repository;
             _mapper = mapper;
             _commandDataClient = commandDataClient;
+            _messageBusClient = messageBusClient;
         }
 
         [HttpGet]
@@ -55,14 +60,27 @@ namespace PlatformService.Controllers
 
             var platformReadDto = _mapper.Map<PlatformReadDto>(platformModel);
 
-            //Sending new Patforn to Command Service
+            //Send Sync Message to Command Service
             try
             {
                 await _commandDataClient.SendPlatformToCommand(platformReadDto);
             }
             catch(Exception ex)
             {
-                Console.WriteLine($"--> Could not send Platform synchronously: {ex.Message} {ex.InnerException}");
+                Console.WriteLine($"--> Could not send to Command Service synchronously: {ex.Message} {ex.InnerException}");
+            }
+
+            //Send Async Message to Command Service
+            try
+            {
+                var platformPublishedDto = _mapper.Map<PlatformPublishedDto>(platformReadDto);
+                platformPublishedDto.Event = "Platform_Published";
+                _messageBusClient.PublishNewPlatform(platformPublishedDto);
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"--> Could not send to Command Service asynchronously: {ex.Message} {ex.InnerException}");
             }
 
             return CreatedAtRoute(nameof(GetPlatformById), new { Id = platformReadDto.Id }, platformReadDto);
